abfd05e25eb766cd987fa2326b327e1a638a663f
ATE <ATE@DESKTOP-ODIPDNU>
2022-08-31 12:22:17 +0900

    master

---
+++ src/main/java/com/gyojincompany/BranchTestApplication.java
@@ -0,0 +1,13 @@
+package com.gyojincompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchTestApplication.class, args);
+	}
+
+}
